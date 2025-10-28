a02094582fe53e8a4772a98f34d93c4204b39b02
Carolbuenodarosa <seuemaildopgithub@gmail.com>
2025-10-27 21:27:02 -0300

    up

--- config/app.php
+++ config/app.php
@@ -65,8 +65,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+    //'timezone' => 'UTC',
+    'timezone' => 'America/Sao_Paulo',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
